214d87b1fa1f58cafb1de2989a013d67d33ce315
Robin Genz <mail@robingenz.dev>
2021-06-05 12:24:36 +0200

    feat(android): implement android (#3)

--- android/src/main/java/dev/robingenz/capacitorjs/plugins/firebase/app/FirebaseApp.java
+++
@@ -1,8 +0,0 @@
-package dev.robingenz.capacitorjs.plugins.firebase.app;
-
-public class FirebaseApp {
-
-    public String echo(String value) {
-        return value;
-    }
-}
--- android/src/main/java/dev/robingenz/capacitorjs/plugins/firebase/app/FirebaseAppPlugin.java
+++ android/src/main/java/dev/robingenz/capacitorjs/plugins/firebase/app/FirebaseAppPlugin.java
@@ -5,18 +5,35 @@ import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 
 @CapacitorPlugin(name = "FirebaseApp")
 public class FirebaseAppPlugin extends Plugin {
 
-    private FirebaseApp implementation = new FirebaseApp();
+    private FirebaseApp firebaseAppInstance;
+
+    public void load() {
+        firebaseAppInstance = FirebaseApp.getInstance();
+    }
 
     @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
-
+    public void getName(PluginCall call) {
         JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
+        ret.put("name", firebaseAppInstance.getName());
+        call.resolve(ret);
+    }
+
+    @PluginMethod
+    public void getOptions(PluginCall call) {
+        FirebaseOptions options = firebaseAppInstance.getOptions();
+        JSObject ret = new JSObject();
+        ret.put("apiKey", options.getApiKey());
+        ret.put("applicationId", options.getApplicationId());
+        ret.put("databaseUrl", options.getDatabaseUrl());
+        ret.put("gcmSenderId", options.getGcmSenderId());
+        ret.put("projectId", options.getProjectId());
+        ret.put("storageBucket", options.getStorageBucket());
         call.resolve(ret);
     }
 }
