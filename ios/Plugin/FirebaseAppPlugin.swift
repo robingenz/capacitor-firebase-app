7a4f92d9f7f2714ded00a9f73c8b8086682ebc2b
Robin Genz <mail@robingenz.dev>
2021-06-06 14:10:37 +0200

    wip

--- ios/Plugin/FirebaseAppPlugin.swift
+++ ios/Plugin/FirebaseAppPlugin.swift
@@ -1,18 +1,35 @@
 import Foundation
 import Capacitor
 
+import FirebaseCore
+
 /**
  * Please read the Capacitor iOS Plugin Development Guide
  * here: https://capacitorjs.com/docs/plugins/ios
  */
 @objc(FirebaseAppPlugin)
 public class FirebaseAppPlugin: CAPPlugin {
-    private let implementation = FirebaseApp()
+    
+    public override func load() {
+        if (FirebaseApp.app() == nil) {
+            FirebaseApp.configure()
+        }
+    }
 
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
+    @objc func getName(_ call: CAPPluginCall) {
         call.resolve([
-            "value": implementation.echo(value)
+            "name": FirebaseApp.app()?.name ?? ""
+        ])
+    }
+    
+    @objc func getOptions(_ call: CAPPluginCall) {
+        call.resolve([
+            "apiKey": FirebaseApp.app()?.options.apiKey ?? "",
+            "applicationId": FirebaseApp.app()?.options.apiKey ?? "",
+            "databaseUrl": FirebaseApp.app()?.options.apiKey ?? "",
+            "gcmSenderId": FirebaseApp.app()?.options.apiKey ?? "",
+            "projectId": FirebaseApp.app()?.options.apiKey ?? "",
+            "storageBucket": FirebaseApp.app()?.options.apiKey ?? "",
         ])
     }
 }
